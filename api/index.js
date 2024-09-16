83d52ffde6b6199a0e338269fb53ec0577b16ec9
Sehaj Dhupia <sehaj.dhupia@gmail.com>
2024-09-16 14:44:31 -0400

    Updated API URL for production

--- api/index.js
+++ api/index.js
@@ -8,7 +8,7 @@ const mongoose = require("mongoose");
 
 //app.use(cors({ origin: '*' }));
 app.use(cors({
-    origin: 'money-tracker-khlpfmmvm-sehaj-dhupias-projects.vercel.app'
+    origin: 'https://money-tracker-ruby.vercel.app'
 }));
 
 
